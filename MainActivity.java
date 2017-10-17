88fb95bb0d5f2534b17bc5ad9aab09a36c8e08cc
Archangel354 <rduncan354@yahoo.com>
2017-10-17 17:56:59 -0400

    added a one line Intent to open the Blade Runner 2049 trailer on YouTube

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.android.youtubesandbox;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Activity mActivity = this;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=gCcx85zbxz4")));
+
     }
 }
